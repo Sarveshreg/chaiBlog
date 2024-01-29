c4219e3d394d1aa53c5751b8597f86772a7f5029
Sarveshreg <sarveshregmi1@gmail.com>
2024-01-29 15:38:32 -0600

    1/29/2024

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
+import Container from "./container/Container";
+import Logo from "./logo";
+import LogoutBtn from "./header/LogoutBtn"
+import Button from "./Button"
+import Input from "./Input"
+
+export {Header,Footer,Container,Logo,LogoutBtn,Button, Input}
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig({server:{
+  watch:{
+    usePolling:true,
+  },
+},
+  plugins: [react({
+    // Add this line
+    include: "**/*.jsx",
+  })]
 })
